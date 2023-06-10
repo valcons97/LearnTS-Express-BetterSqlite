dbcd0a4bfaa7bfca5d0124923704183e0b9e5a7c
valcons97 <jonathanadi46@gmail.com>
2023-06-10 21:29:28 +0700

    feat: setup express server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+const port = 9000;
+
 app.get("/", (req: Request, res: Response) => {
     return res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
 })
