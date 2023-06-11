cb8e6ad6797af226c5bc26aa5a31105ef282a206
valcons97 <jonathanadi46@gmail.com>
2023-06-12 01:24:05 +0700

    feat: update readme

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,37 +1,37 @@
 import express, { Request, Response } from 'express';
 import model from '../model/todo_model'
 
-const getTodo = async (req: Request, res: Response) => {
-    try {
-        const results = await model.getTodo();
-        return res.status(200).json({
-            status: "OK",
-            message: "Sucesfully get todo",
-            results
-        })
-    } catch (e) {
-        return res.status(500).json({ msg: "fail to get", })
+class Controller {
+    getTodo = async (req: Request, res: Response) => {
+        try {
+            const results = await model.getTodo();
+            return res.status(200).json({
+                status: "OK",
+                message: "Sucesfully get todo",
+                results
+            })
+        } catch (e) {
+            return res.status(500).json({ msg: "fail to get", })
+        }
     }
 
+    createTodo = async (req: Request, res: Response) => {
+        try {
+            const results = await model.create(req.body.title,);
+            res.status(201).json({
+                message: "Sucesfully create todo",
+            })
+        } catch (e) {
+            return res.status(500).json({ msg: "fail to create", })
+        }
 
-
-}
-
-
-const createTodo = async (req: Request, res: Response) => {
-    try {
-        const results = await model.create(req.body.title,);
-        res.status(201).json({
-            message: "Sucesfully create todo",
-        })
-    } catch (e) {
-        return res.status(500).json({ msg: "fail to create", })
     }
-
 }
 
 
-export = {
-    getTodo,
-    createTodo,
-}
+
+
+
+
+
+export default new Controller();
